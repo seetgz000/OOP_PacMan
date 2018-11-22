c63b39bc411abe86d79a000ca2a020ce138cee79
ZiZhao <nerotomeikun@gmail.com>
2018-11-22 18:21:49 +0800

    quitbtn

--- src/main/scala/OOP_PacMan/Main.scala
+++ src/main/scala/OOP_PacMan/Main.scala
@@ -1,23 +1,17 @@
 package OOP_PacMan
 
-import java.io.File
-
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.{Group, Node, Parent, Scene}
+import scalafx.scene.{Parent, Scene}
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
 import scalafx.Includes._
 import javafx.{scene => jfxs}
 import scalafx.scene.text.Font
 import scalafx.stage.{Modality, Stage}
 import Database.Database
-import OOP_PacMan.controller.{PlayGameController, PlayerPageController}
+import OOP_PacMan.controller.PlayerPageController
 import User.Players
-import scalafx.beans.property.DoubleProperty
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.image.{Image, ImageView}
-import scalafx.scene.input.KeyCode
-import scalafx.scene.layout.AnchorPane
 
 object Main extends JFXApp {
 
@@ -57,66 +51,7 @@ object Main extends JFXApp {
     stage.scene().setRoot(roots2)
     stage.setMaxWidth(423)
     stage.setMinWidth(423)
-
-    showGameCanvas(roots2)
-
   }
-
-  def showGameCanvas(root: AnchorPane): Unit = {
-
-    val group = new Group() {
-      /** Coordinate of Pacman */
-      val pacmanX = new DoubleProperty
-      val pacmanY = new DoubleProperty
-
-      /** Initialize Pacman */
-      val pacmanImg = new Image(new File("src/main/resource/OOP_PacMan/image/pacmanGIF(fast).gif").toURI.toURL.toString)
-      val pacmanW = 40
-      val pacmanH = 40
-
-
-      val pacman = new ImageView(pacmanImg) {
-        fitWidth = 40
-        preserveRatio = true
-        x = 100
-        y = 100
-        translateX <== pacmanX
-        translateY <== pacmanY
-      }
-      stage.scene().onKeyPressed = k => k.code match {
-
-        case KeyCode.W =>
-
-          //        canvas.translateY = canvas.translateY.value - 6
-          pacmanY() = pacmanY.value - 6
-          print(pacmanX, pacmanY)
-
-        case KeyCode.A =>
-          //        canvas.translateX= canvas.translateX.value - 6
-          pacmanX() = pacmanX.value - 6
-          print(pacmanX, pacmanY)
-
-        case KeyCode.S =>
-          //        canvas.translateY = canvas.translateY.value + 6
-          pacmanY() = pacmanY.value + 6
-          print(pacmanX, pacmanY)
-
-        case KeyCode.D =>
-          //        canvas.translateX = canvas.translateX.value + 6
-          pacmanX() = pacmanX.value + 6
-          print(pacmanX, pacmanY)
-
-        case _ =>
-
-      }
-
-      children = List(
-        pacman
-      )
-    }
-    root.getChildren.add(group)
-  }
-
 
   //show high score page
   def showHighScore():Unit ={
